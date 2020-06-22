fafe2ca0e716b4bc240c213f0ae50c698817ab8d
Eray Hanoğlu <erayszn@gmx.net>
2020-06-22 18:00:36 +0300

    Added typings

---
+++ lib/index.d.ts
@@ -0,0 +1,21 @@
+/// <reference types="node" />
+import {EventEmitter} from "events";
+
+declare module "putil-callable" {
+
+
+    class Callable {
+        constructor(callProperty: string);
+    }
+
+    class CallableEventEmitter extends EventEmitter {
+        constructor(callProperty: string);
+    }
+
+    interface Callable extends Function {
+    }
+
+    interface CallableEventEmitter extends EventEmitter, Function {
+    }
+
+}
